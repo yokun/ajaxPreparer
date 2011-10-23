a6eaca910cdc851e9fae21b6682cd96651d8894b
Cary Landholt <cary@landholt.com>
2011-10-22 23:55:58 -0500

    Moved some functions out of the handler, utilizing the closure

--- jquery-ajaxPreparer-handler.js
+++ jquery-ajaxPreparer-handler.js
@@ -1,6 +1,6 @@
 /*!
  * jquery-ajaxPreparer-handler
- * Version:  0.9.3
+ * Version:  0.9.4
  * Source:  https://github.com/CaryLandholt/jquery-ajaxPreparer-handler
  */
 
@@ -9,41 +9,60 @@
 (function ($, handlers) {
 	'use strict';
 
-	handlers.ajaxPreparer = function (e, options) {
-		var base = this,
-			validTagNames = ['A', 'INPUT', 'SELECT'],
-			ajaxOptions = {},
-			$form;
+	// only acceptable ajaxable trigger elements
+	var validTagNames = ['A', 'INPUT', 'SELECT'];
 
+	// gets ajax options from the element
+	function getAjaxOptions(base) {
+		var $form;
+
+		// anchor was the trigger element
+		if (base.el.tagName === 'A') {
+			return {
+				type: 'get',
+				url: base.$el.prop('href')
+			};
+		}
+
+		// form will be used as the trigger element
+		$form = base.$el.parents('form');
+
+		return {
+			type: $form.prop('method'),
+			url: $form.prop('action'),
+			data: $form.serialize()
+		};
+	}
+
+	handlers.ajaxPreparer = function (e, options) {
+		// To avoid scope issues, use 'base' instead of 'this'
+		// to reference this class from internal events and functions.
+		var base = this,
+			ajaxOptions = {};
+
+		// prevent the default browser handler from firing
+		// necessary for anchors and buttons
 		e.preventDefault();
 
+		// Access to DOM and jQuery versions of the element
 		base.el = e.target;
 		base.$el = $(base.el);
-		base.handlerOptions = options;
-		base.metadata = base.$el.data(handlers.ajaxPreparer.defaults.metadatakey);
-		base.$el.data(handlers.ajaxPreparer.defaults.dataStorageName, base);
-		base.settings = $.extend(true, {}, handlers.ajaxPreparer.defaults, base.handlerOptions, base.metadata);
 
+		// verify if the elements tagName is valid
 		if ($.inArray(base.el.tagName, validTagNames) === -1) {
 			base.$el.trigger(base.settings.invalidAjaxTagEventName, base.el.tagName);
 
 			return false;
 		}
 
-		if (base.el.tagName === 'A') {
-			$.extend(ajaxOptions, {
-				type: 'get',
-				url: base.$el.prop('href')
-			});
-		} else {
-			$form = base.$el.parents('form');
+		base.handlerOptions = options;
+		base.metadata = base.$el.data(handlers.ajaxPreparer.defaults.metadatakey);
 
-			$.extend(ajaxOptions, {
-				type: $form.prop('method'),
-				url: $form.prop('action'),
-				data: $form.serialize()
-			});
-		}
+		// Add a reverse reference to the DOM object
+		base.$el.data(handlers.ajaxPreparer.defaults.dataStorageName, base);
+		base.settings = $.extend(true, {}, handlers.ajaxPreparer.defaults, base.handlerOptions, base.metadata);
+
+		$.extend(ajaxOptions, getAjaxOptions(base));
 
 		// retain context throughout successive event calls
 		ajaxOptions.context = base.el;
