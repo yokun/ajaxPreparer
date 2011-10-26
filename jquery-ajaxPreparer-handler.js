068f7c8d3a2826c3531fbbc4dc35d80f207185a8
Cary Landholt <cary@landholt.com>
2011-10-25 23:19:53 -0500

    Added comments
    Moved preventDefault to the first executable line inside the handler

--- jquery-ajaxPreparer-handler.js
+++ jquery-ajaxPreparer-handler.js
@@ -1,7 +1,50 @@
 /*!
  * jquery-ajaxPreparer-handler
- * Version:  0.9.6
+ * Version:  0.9.7
  * Source:  https://github.com/CaryLandholt/jquery-ajaxPreparer-handler
+ *
+ * Copyright (c) 2011 Cary Landholt
+ * https://github.com/CaryLandholt
+ * https://twitter.com/CaryLandholt
+ *
+ * Description
+ * Prepare ajax calls through conventions.  Ajaxify anchors (links), inputs, and selects.
+ * This handler's sole responsibility is to prepare the ajax call.
+ * Upon preparation an event will be fired for consumption by another handler.
+ *
+ * Registration
+ *	$(document)
+ *		.on('click', 'form[data-ajax] :submit, a[data-ajax]', $.handlers.ajaxPreparer)
+ *		.on('change', 'input[data-ajax], select[data-ajax]', $.handlers.ajaxPreparer);
+ *
+ * Usage
+ * http://jsfiddle.net/carylandholt/24gsA/
+ * With the above suggested registration pattern, inputs and selects with the [data-ajax] attribute
+ * will be handled on the change event.  Anchors and submits will be handled on the click event.
+ * 
+ *	<a href="/echo/html/"
+ *		data-ajax="true"
+ *		data-ajax-options='{"ajaxOptions" : {"data": {"html": "<div>My HTML</div>", "delay": 1}}}'>HTML</a>
+ *
+ *	<a
+ *		href="/echo/json/"
+ *		data-ajax="true"
+ *		data-ajax-options='{"ajaxOptions" : {"type" : "post", "dataType" : "json", "data": "json={\"foo\" : \"bar\"}"}}'>JSON</a>
+ *
+ *	<form method="post" action="/echo/html/" data-ajax="true">
+ *		<input type="text" name="FirstName" value="My First Name" data-ajax="true" />
+ *		<input type="text" name="LastName" value="My Last Name" />
+ *
+ *		<select name="Gender" data-ajax="true">
+ *			<option value="M">Male</option>
+ *			<option value="F">Female</option>
+ *		</select>
+ *
+ *		<input type="checkbox" name="IsActive" data-ajax="true" />
+ *		<input type="hidden" name="IsActive" value="false" />
+ *
+ *		<input type="submit" value="Submit" />
+ *	</form>
  */
 
 /*global jQuery*/
@@ -24,7 +67,7 @@
 			};
 		}
 
-		// form will be used as the trigger element
+		// form will be used for type, url and data
 		$form = base.$el.parents('form');
 
 		return {
@@ -35,24 +78,25 @@
 	}
 
 	handlers.ajaxPreparer = function (e, options) {
+		// Prevent the default browser handler from firing.
+		// Necessary for anchors and buttons.
+		e.preventDefault();
+
 		// To avoid scope issues, use 'base' instead of 'this'
 		// to reference this class from internal events and functions.
 		var base = this,
 			ajaxOptions = {};
-
-		// prevent the default browser handler from firing
-		// necessary for anchors and buttons
-		e.preventDefault();
 
 		// exit if there is no event
 		if (!e) {
 			return;
 		}
 
-		// Access to DOM and jQuery versions of the element
+		// Access to DOM version of the element
 		base.el = e.target;
+		// Access to jQuery version of the element
 		base.$el = $(base.el);
-
+		// Access to 
 		base.handlerOptions = options;
 		base.metadata = base.$el.data(handlers.ajaxPreparer.defaults.metadatakey);
 
@@ -64,7 +108,7 @@
 		if ($.inArray(base.el.tagName, validTagNames) === -1) {
 			base.$el.trigger(base.settings.invalidAjaxTagEventName, base.el.tagName);
 
-			return false;
+			return;
 		}
 
 		$.extend(ajaxOptions, getAjaxOptions(base));
@@ -82,13 +126,3 @@
 		invalidAjaxTagEventName: 'invalid-ajax-tag'
 	};
 }(jQuery, jQuery.handlers = jQuery.handlers || {}));
-
-/*
-$(document).on('click', 'form[data-ajax="true"] :submit', $.handlers.ajaxPreparer);
-
-$(document).on('click', 'a[data-ajax="true"]', function (e) {
-	return $.handlers.ajaxPreparer(e, {ajaxOptions: {foo: 'bar'}});
-});
-
-$(document).on('change', 'input[data-ajax="true"], select[data-ajax="true"]', $.handlers.ajaxPreparer);
-*/
