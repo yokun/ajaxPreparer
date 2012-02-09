d7409fea3ef6d2ea6ea0490121013705a4f08635
Cary Landholt <cary@landholt.com>
2012-02-08 20:19:54 -0600

    add submodules subfolder - cleanup

--- test/main.js
+++ test/main.js
@@ -6,16 +6,8 @@
 	require.config({
 		paths: {
 			ajaxPreparer: '../ajaxPreparer',
-			doc: 'doc/doc',
-			document: 'document/document',
-			jquery: 'jquery-build/dist/jquery',
-			publish: 'publish/publish',
-			pubsub: 'pubsub/pubsub',
-			'qunit-official': 'qunit/qunit/qunit',
-			qunit: 'qunit-module-patch/qunit-module-patch',
-			subscribe: 'subscribe/subscribe',
-			unsubscribe: 'unsubscribe/unsubscribe',
-			window: 'window/window'
+			'qunit-official': 'qunit',
+			qunit: 'qunit-module-patch'
 		}
 	});
 
